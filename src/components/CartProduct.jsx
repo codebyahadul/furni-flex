2ebfabe525d0b7709e80e75dd63f3cf4326e4221
codebyahadul <ahadul.prof@gmail.com>
2024-09-06 15:50:45 +0600

    added cart page and show the cart product

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,7 +1,34 @@
-const CartProduct = () => {
+import { IoClose } from "react-icons/io5";
+
+/* eslint-disable react/prop-types */
+const CartProduct = ({ product }) => {
     return (
-        <div>
-            
+        <div className="py-5 px-10 relative space-y-3 border-b-2">
+            <div className="flex items-center gap-3">
+                <div className="p-3 bg-white border rounded-md">
+                    <button
+                        className="rounded-md px-2"
+                    >
+                        <b className="text-lg">-</b>
+                    </button>
+                    <button className="rounded">{product.quantity}</button>
+                    <button
+                        className="rounded-md px-2"
+                    >
+                        <b className="text-lg">+</b>
+                    </button>
+                </div>
+                <div>
+                    <img className="size-32" src={product?.thumbnail} alt="" />
+                </div>
+                <div>
+                    <h1 className="text-lg font-medium">{product?.title}</h1>
+                </div>
+            </div>
+            <div>
+                <button className="absolute top-1 right-2"><IoClose size={30} /></button>
+                <p className="absolute bottom-3 right-4 text-xl font-medium">${product?.offerPrice}</p>
+            </div>
         </div>
     );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,40 @@
 import { useContext } from "react";
 import { CartContext } from "../providers/ContextProvider";
+import CartProduct from "../components/CartProduct";
 
 const Cart = () => {
-    const {cart} = useContext(CartContext)
-    console.log(cart);
+    const { cart } = useContext(CartContext)
     return (
-        <div className="flex gap-5 my-5 md:my-8">
-            <div>
+        <div className="flex flex-col md:flex-row gap-5 lg:gap-10 my-5 md:my-8">
+            <div className="flex-1">
                 <h1 className="text-xl md:text-2xl font-medium">An overview of your order</h1>
-                <div>
-
+                <div className="my-5 bg-gray-100 rounded-lg">
+                    {
+                        cart.map((product, idx) => <CartProduct key={idx} product={product} />)
+                    }
                 </div>
+            </div>
+            <div>
+                <h1 className="text-xl md:text-2xl font-medium">Oder details</h1>
+                <div className="bg-gray-100 px-10 py-5 my-5 w-full lg:w-[400px] rounded-lg space-y-3 border-2 border-gray-200">
+                    <div className="flex justify-between text-lg font-medium">
+                        <h1>Subtotal</h1>
+                        <p>$1200</p>
+                    </div>
+                    <div className="flex justify-between text-lg font-medium">
+                        <h1>Shipping</h1>
+                        <p>Free</p>
+                    </div>
+                    <div className="flex justify-between text-lg font-medium">
+                        <h1>Estimated Tax</h1>
+                        <p>$00</p>
+                    </div>
+                    <div className="flex justify-between text-xl font-bold border-t border-b border-gray-200 py-3">
+                        <h1>Total</h1>
+                        <p>$1200</p>
+                    </div>
+                </div>
+                <button className="text-lg font-medium text-white py-2 bg-black rounded-md w-full">Go to Checkout</button>
             </div>
         </div>
     );
